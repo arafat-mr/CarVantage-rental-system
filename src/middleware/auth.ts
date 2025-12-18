a74e9f09c24cdda86765d82e251baf5bc9381fed
arafat-mr <arafatmr42@gmail.com>
2025-12-18 15:58:51 +0400

    fixed error

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -9,7 +9,9 @@ const auth = (...roles:string[]) => {
     
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const token = req.headers.authorization;
+      const token = req.headers.authorization?.split(' ')[1];
+     
+      
       if (!token) {
         return res.status(500).json({ message: "You are not allowed" });
       }
